de78076d36cb18fda169181ff0156a29bde410a6
劉展宏 <>
2023-02-10 19:07:21 +0800

    no message

--- controllers/note.php
+++ controllers/note.php
@@ -5,7 +5,7 @@ $db = new Database($config['database']);
  $heading = 'Note';
 
  $id = $_GET['id'];
-
+//DB
  $notes = $db->query('select * from notes where id = ?', [$id])->fetch();
  //  $notes = $db->query('select * from notes where id = id', ['id' => $id])->fetchAll();
 //  $notes = $db->query('select * from notes where id = :id', [':id' => $id])->fetchAll();
