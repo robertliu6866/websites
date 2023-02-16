2781b60e0db283ec68d6e64be332329dcb202488
劉展宏 <>
2023-02-17 00:17:12 +0800

    no message

---
+++ views/403.php
@@ -0,0 +1,17 @@
+<?php require ('partials/head.php')   ?>
+<?php require ('partials/nav.php')   ?>
+
+
+
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <!-- Replace with your content -->
+   <h1 class="text-2xl font-bold"> 喔喔！！沒有權限查看此內容！</h1>
+  <img src="https://scontent.ftpe7-1.fna.fbcdn.net/v/t39.30808-6/278217986_10227073769180293_3974764083770939990_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=okOtP9xFxrwAX_q4gJF&_nc_ht=scontent.ftpe7-1.fna&oh=00_AfCvlmGumiYQFomuaZvdcN-TakxpMIRLzs0kSlyMaBSwNg&oe=63F273C7" alt="">
+   <p class="mt-4">
+    <a href="/" class="text-blue underline">回首頁</a>
+</p>
+      <!-- /End replace -->
+    </div>
+  </main>
+  <?php require ('partials/footer.php')   ?>
