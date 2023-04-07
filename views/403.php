c8cacbe6b35bc446d98edfcd920d686e9f00039d
劉展宏 <48748503+robertliu6866@users.noreply.github.com>
2023-04-08 03:28:11 +0800

    Update 403.php

--- views/403.php
+++ views/403.php
@@ -6,7 +6,7 @@
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <!-- Replace with your content -->
-   <h1 class="text-2xl font-bold"> 喔喔！！沒有權限！！!!!!！</h1>
+   <h1 class="text-2xl font-bold"> 喔喔！！沒有權限！！!!!!</h1>
   <img src="https://megapx.dcard.tw/v1/images/0b02be5a-1230-4227-ba1f-03772e13ef23/responsive?width=640" alt="">
    <p class="mt-4">
     <a href="/" class="text-blue underline">回首頁</a>
